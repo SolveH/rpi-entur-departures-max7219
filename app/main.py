240bb2651f0304d6f4547763f53e1bf535f37c10
Sølve Bø Hunvik <Solve.Bo.Hunvik@avinor.no>
2025-11-19 22:18:50 +0100

    test adding ø to original font

--- app/main.py
+++ app/main.py
@@ -97,7 +97,13 @@ if __name__ == "__main__":
     if False:  # TODO: legge til sjekk på om det kjøres på noko anna enn pi
         display_text_on_target_device(display_text_next_departure, width=40, delay=0.07)
 
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf", 8)
+    MY_FONT = {
+        **LCD_FONT,
+        'ø': [0x00, 0x36, 0x49, 0x49, 0x49, 0x36, 0x00],  # Example bitmap
+        # Add 'æ', 'å', etc.
+    }
+
+    font = proportional(MY_FONT)
     text = display_text_next_departure
     bbox = font.getbbox(text)
     text_width = bbox[2] - bbox[0]
