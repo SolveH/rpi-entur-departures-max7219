fe40c491ace857640b30731538dedec82bf82649
Sølve Bø Hunvik <srb.hunvik@gmail.com>
2025-11-20 23:45:46 +0100

    refactor client and add get stop places quays query for future purposes

--- entur_client.py
+++ entur_client.py
@@ -6,14 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ET_CLIENT_NAME = os.environ["ET_CLIENT_NAME"]
+JOURNEY_PLANNER_API_URL = "https://api.entur.io/journey-planner/v3/graphql"
+HEADERS = {
+    "Content-Type": "application/json",
+    "ET-Client-Name": ET_CLIENT_NAME
+}
 
+def call_journey_planner_api(query: str) -> dict:
+    response = requests.post(JOURNEY_PLANNER_API_URL, json={"query": query}, headers=HEADERS)
+    return response.json()
 
 def get_estimated_calls_for_quay(quay_id: str) -> list:
-    url = "https://api.entur.io/journey-planner/v3/graphql"
-    headers = {
-        "Content-Type": "application/json",
-        "ET-Client-Name": ET_CLIENT_NAME
-    }
     time_range = 7200
     number_of_departures = 5
     query = """
@@ -36,8 +39,38 @@ def get_estimated_calls_for_quay(quay_id: str) -> list:
       }
     }
     """ % (quay_id, time_range, number_of_departures)
-    response = requests.post(url, json={"query": query}, headers=headers)
-    data = response.json()
-    expected_departures = data["data"]["quay"]["estimatedCalls"]
+    data = call_journey_planner_api(query)
 
-    return expected_departures
+    return data["data"]["quay"]["estimatedCalls"]
+
+def get_stop_place_quays(stop_place_id: str) -> dict:
+    time_range = 1800
+    number_of_departures = 10
+    query = """
+    {
+      stopPlace(id: "%s") {
+        id
+        name
+        quays {
+          id
+          name
+          estimatedCalls(timeRange: %s, numberOfDepartures: %s) {
+            realtime
+            aimedDepartureTime
+            expectedDepartureTime
+            destinationDisplay {
+              frontText
+            }
+            serviceJourney {
+              line {
+                id
+                publicCode
+              }
+            }
+          }
+        }
+      }
+    }
+    """ % (stop_place_id, time_range, number_of_departures)
+    data = call_journey_planner_api(query)
+    return data["data"]["stopPlace"]["quays"]
